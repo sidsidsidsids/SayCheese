88b873f8724b9e129d4b8f4af5278405e2f5bdac
김세혁 <se6816@naver.com>
2023-07-17 16:20:52 +0900

    Init:예외 처리 초기 세팅

---
+++ BackEnd/article/src/main/java/com/reminiscence/article/exception/BaseException.java
@@ -0,0 +1,13 @@
+package com.reminiscence.article.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BaseException extends RuntimeException{
+    private final HttpStatus httpStatus;
+    public BaseException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
---
+++ BackEnd/article/src/main/java/com/reminiscence/article/exception/ControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.reminiscence.article.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse response=ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(),"서버와 연결이 원활하지 않습니다.");
+        return new ResponseEntity<ErrorResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
