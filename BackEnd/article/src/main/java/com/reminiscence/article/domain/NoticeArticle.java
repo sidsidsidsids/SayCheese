45072641e429801c91cd3e608bcf047274ab7512
김세혁 <se6816@naver.com>
2023-07-18 12:26:10 +0900

    Change: Notice 엔티티 일부 수정

--- BackEnd/article/src/main/java/com/reminiscence/article/domain/NoticeArticle.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/domain/NoticeArticle.java
@@ -1,13 +1,11 @@
 package com.reminiscence.article.domain;
 
-import com.reminiscence.article.domain.Article;
-
 import javax.persistence.*;
 
 @Entity
 @DiscriminatorValue("N")
 @Table(name = "notice_article")
-public class Notice extends Article {
+public class NoticeArticle extends Article {
     @Column(name = "subject")
     private String subject;
     @Column(name = "content")
