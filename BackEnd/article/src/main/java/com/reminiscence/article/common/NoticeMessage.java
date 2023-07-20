da9bcb3da2647218aa85614e66b8d85278bc6676
김세혁 <se6816@naver.com>
2023-07-21 08:40:46 +0900

    Rename: MessageCode NoticeMessage로 이름 변경

--- BackEnd/article/src/main/java/com/reminiscence/article/common/NoticeMessage.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/common/NoticeMessage.java
@@ -1,21 +1,18 @@
 package com.reminiscence.article.common;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 @Getter
-public enum MessageCode {
+public enum NoticeMessage {
 
     NOTICE_WRITE_SUCCESS(HttpStatus.OK, "공지글 작성이 완료되었습니다."),
     NOTICE_MODIFY_SUCCESS(HttpStatus.OK, "공지글 수정이 완료되었습니다.");
 
 
-    MessageCode(HttpStatus httpStatus, String message) {
+    NoticeMessage(HttpStatus httpStatus, String message) {
         this.httpStatus = httpStatus;
         this.message = message;
     }
--- BackEnd/article/src/main/java/com/reminiscence/article/notice/controller/NoticeController.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/notice/controller/NoticeController.java
@@ -1,14 +1,12 @@
 package com.reminiscence.article.notice.controller;
 
-import com.reminiscence.article.common.MessageCode;
+import com.reminiscence.article.common.NoticeMessage;
 import com.reminiscence.article.config.auth.UserDetail;
-import com.reminiscence.article.domain.NoticeArticle;
 import com.reminiscence.article.notice.dto.NoticeArticleAndMemberRequestDto;
 import com.reminiscence.article.notice.dto.NoticeArticleRequestDto;
 import com.reminiscence.article.notice.dto.NoticeArticleResponseDto;
 import com.reminiscence.article.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -18,7 +16,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,21 +31,21 @@ public class NoticeController {
      * @return
      */
     @PostMapping("")
-    public ResponseEntity<MessageCode.Response> writeNoticeArticle(@AuthenticationPrincipal UserDetail userDetail,
-                                                                   @Valid @RequestBody NoticeArticleRequestDto noticeArticleRequestDto) throws Exception{
+    public ResponseEntity<NoticeMessage.Response> writeNoticeArticle(@AuthenticationPrincipal UserDetail userDetail,
+                                                                     @Valid @RequestBody NoticeArticleRequestDto noticeArticleRequestDto) throws Exception{
         // user 정보와 사용자 요청 정보를 하나의 Dto로 만듬
         NoticeArticleAndMemberRequestDto noticeArticleAndMemberRequestDto=NoticeArticleAndMemberRequestDto.builder()
                 .member(userDetail.getMember())
                 .noticeArticleRequestDto(noticeArticleRequestDto).build();
         noticeService.writeArticle(noticeArticleAndMemberRequestDto);
-        return MessageCode.NOTICE_WRITE_SUCCESS.toResponseEntity();
+        return NoticeMessage.NOTICE_WRITE_SUCCESS.toResponseEntity();
     }
     @PutMapping("/{noticeArticleId}")
-    public ResponseEntity<MessageCode.Response> modifyNoticeArticle(@PathVariable String noticeArticleId,
-                                                                   @Valid @RequestBody NoticeArticleRequestDto noticeArticleRequestDto) throws Exception{
+    public ResponseEntity<NoticeMessage.Response> modifyNoticeArticle(@PathVariable String noticeArticleId,
+                                                                      @Valid @RequestBody NoticeArticleRequestDto noticeArticleRequestDto) throws Exception{
         // user 정보와 사용자 요청 정보를 하나의 Dto로 만듬
         noticeService.modifyArticle(Long.parseLong(noticeArticleId),noticeArticleRequestDto);
-        return MessageCode.NOTICE_MODIFY_SUCCESS.toResponseEntity();
+        return NoticeMessage.NOTICE_MODIFY_SUCCESS.toResponseEntity();
     }
     @GetMapping("")
     public ResponseEntity<NoticeArticleResponseDto> getNoticeArticleList(@PageableDefault(size=10, page=1, direction = Sort.Direction.DESC) Pageable pageable) throws Exception{
