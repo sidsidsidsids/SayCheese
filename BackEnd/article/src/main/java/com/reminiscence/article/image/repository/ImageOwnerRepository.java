3bfb04087511a910a1302ea58c17d4da0a6b293e
이찬형 <cksgud410@gmail.com>
2023-07-21 16:08:49 +0900

    Feat : 이미지 소유자 삭제 기능

--- BackEnd/article/src/main/java/com/reminiscence/article/image/repository/ImageOwnerRepository.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/image/repository/ImageOwnerRepository.java
@@ -2,6 +2,7 @@ package com.reminiscence.article.image.repository;
 
 import com.reminiscence.article.domain.ImageOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,13 +11,18 @@ import java.util.List;
 
 @Repository
 public interface ImageOwnerRepository extends JpaRepository<ImageOwner, Long> {
+
     @Query("select i from ImageOwner i where i.image.id = :imageId and i.member.id = :memberId")
-    public ImageOwner findByImageIdAndOwnerId(Long imageId, Long memberId);
+    public ImageOwner findByImageIdAndMemberId(@Param("imageId") Long imageId, @Param("memberId") Long memberId);
 
     @Query("select i from ImageOwner i where i.member.id = :memberId")
     public List<ImageOwner> findByMemberId(@Param("memberId") Long memberId);
 
     @Query("select i from ImageOwner i where i.image.id = :imageId")
     public List<ImageOwner> findByImageId(@Param("imageId") Long imageId);
+
+    @Modifying
+    @Query("delete from ImageOwner i where i.image.id = :imageId and i.member.id = :memberId")
+    public void deleteByImageIdAndMemberId(@Param("imageId") Long imageId, @Param("memberId") Long memberId);
 
 }
