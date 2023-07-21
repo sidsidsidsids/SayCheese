42758c1468cf6a3bc5f9eea8328beda70100cb94
이찬형 <cksgud410@gmail.com>
2023-07-21 16:08:04 +0900

    Feat : 게시글 좋아요 Entity 추가

--- BackEnd/article/src/main/java/com/reminiscence/article/domain/Image.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/domain/Image.java
@@ -2,6 +2,7 @@ package com.reminiscence.article.domain;
 
 import com.reminiscence.article.BaseTimeEntity;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.util.ArrayList;
