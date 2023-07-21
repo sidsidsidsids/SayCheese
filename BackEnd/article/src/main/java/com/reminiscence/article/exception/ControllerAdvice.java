029ea80dce2cc1ec40292090a359ca8e691633a6
이찬형 <cksgud410@gmail.com>
2023-07-21 16:07:20 +0900

    Feat : ImageException, ImageArticleException 추가

--- BackEnd/article/src/main/java/com/reminiscence/article/exception/ControllerAdvice.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/exception/ControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.reminiscence.article.exception;
 
 
+import com.reminiscence.article.exception.customexception.ImageArticleException;
+import com.reminiscence.article.exception.customexception.ImageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,16 @@ public class ControllerAdvice {
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         ErrorResponse response=ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(),"서버와 연결이 원활하지 않습니다.");
         return new ResponseEntity<ErrorResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleImageException(ImageException imageException) {
+        ErrorResponse response=ErrorResponse.of(imageException.getHttpStatus().value(),imageException.getMessage());
+        return new ResponseEntity<ErrorResponse>(response, imageException.getHttpStatus());
+    }
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleImageArticleException(ImageArticleException imageArticleException) {
+        ErrorResponse response=ErrorResponse.of(imageArticleException.getHttpStatus().value(),imageArticleException.getMessage());
+        return new ResponseEntity<ErrorResponse>(response, imageArticleException.getHttpStatus());
     }
 
 }
