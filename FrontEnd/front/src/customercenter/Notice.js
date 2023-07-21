399c3c3df4fe48b3ea20f1f7d9497de4ac43c416
이지원 <dlwldnjs427@naver.com>
2023-07-21 16:54:58 +0900

    [FE] 고객센터의 공지사항 목록, 상세 페이지, 자주 묻는 질문 구조 잡기

---
+++ FrontEnd/front/src/customercenter/Notice.js
@@ -0,0 +1,29 @@
+import "./Notice.css";
+
+// db 연결 시 제목, 작성일, 조회수, 내용 수정해야 함
+function Notice() {
+  return (
+    <div>
+      <h1>공지사항</h1>
+      <br className="stop-dragging" />
+      <hr className="NoticeLine" />
+      <h2>제목1</h2>
+      <div className="NoticeInfo">
+        <h4>관리자</h4>
+        <div className="NoticeInfo">
+          <h4>2023.07.17</h4>
+          <h4>&nbsp;&nbsp;조회 255</h4>
+        </div>
+      </div>
+      <hr className="NoticeLine" />
+      <div className="NoticeMain">
+        <br className="stop-dragging" />
+        내용내용
+        <br />
+        어쩌구저쩌구~~~
+      </div>
+      <hr className="NoticeLine" />
+    </div>
+  );
+}
+export default Notice;
