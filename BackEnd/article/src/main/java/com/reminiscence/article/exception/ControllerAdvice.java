7e9ad9b6c01d51ddbfef1f3aafc3f7d831aeb7c4
이찬형 <cksgud410@gmail.com>
2023-07-24 02:18:26 +0900

    Feat : 유저 관련 CustomException 추가

--- BackEnd/article/src/main/java/com/reminiscence/article/exception/ControllerAdvice.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/exception/ControllerAdvice.java
@@ -3,6 +3,7 @@ package com.reminiscence.article.exception;
 
 import com.reminiscence.article.exception.customexception.ImageArticleException;
 import com.reminiscence.article.exception.customexception.ImageException;
+import com.reminiscence.article.exception.customexception.MemberException;
 import com.reminiscence.article.exception.customexception.NoticeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +18,23 @@ public class ControllerAdvice {
         ErrorResponse response=ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(),"서버와 연결이 원활하지 않습니다.");
         return new ResponseEntity<ErrorResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    // Image 관련 예외 처리
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleImageException(ImageException imageException) {
         ErrorResponse response=ErrorResponse.of(imageException.getHttpStatus().value(),imageException.getMessage());
         return new ResponseEntity<ErrorResponse>(response, imageException.getHttpStatus());
     }
+    // ImageArticle 관련 예외 처리
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleImageArticleException(ImageArticleException imageArticleException) {
         ErrorResponse response=ErrorResponse.of(imageArticleException.getHttpStatus().value(),imageArticleException.getMessage());
         return new ResponseEntity<ErrorResponse>(response, imageArticleException.getHttpStatus());
+    }
+    // Member 관련 예외 처리
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleMemberException(MemberException memberException) {
+        ErrorResponse response = ErrorResponse.of(memberException.getHttpStatus().value(), memberException.getMessage());
+        return new ResponseEntity<ErrorResponse>(response, memberException.getHttpStatus());
     }
 
     // Notice 관련 예외 처리
