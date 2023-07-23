74c4527c8d6cf24752ab8df4f8d2dcc927270c4f
이찬형 <cksgud410@gmail.com>
2023-07-24 02:37:31 +0900

    Feat : 이미지 게시글 태그 관련 Exception Message 추가

--- BackEnd/article/src/main/java/com/reminiscence/article/exception/message/ImageExceptionMessage.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/exception/message/ImageExceptionMessage.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ImageExceptionMessage {
-    NOT_FOUND_IMAGE("해당 이미지를 찾을 수 없습니다.",HttpStatus.NOT_FOUND);
+
+    NOT_FOUND_IMAGE("해당 이미지를 찾을 수 없습니다.",HttpStatus.NOT_FOUND),
+    NOT_FOUND_IMAGE_TAG("해당 이미지의 태그를 찾을 수 없습니다.",HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus httpStatus;
