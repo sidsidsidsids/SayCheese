f17fa6ee6e6eb283cae2102f34262a8fad40644f
김세혁 <se6816@naver.com>
2023-07-24 16:35:08 +0900

    Fix: gradle mavenBom 인식 못하는 오류 해결

--- BackEnd/article/src/main/java/com/reminiscence/article/ArticleApplication.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/ArticleApplication.java
@@ -2,10 +2,12 @@ package com.reminiscence.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class ArticleApplication {
 
 	public static void main(String[] args) {
