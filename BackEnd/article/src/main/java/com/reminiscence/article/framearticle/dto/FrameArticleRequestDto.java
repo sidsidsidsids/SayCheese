98f3f9be0de447b865c13df69a3bbb8f5fff0b55
김세혁 <se6816@naver.com>
2023-07-26 12:27:36 +0900

    Change: FrameArticleRequestDto @Vaild 제약 조건 변경

--- BackEnd/article/src/main/java/com/reminiscence/article/framearticle/dto/FrameArticleRequestDto.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/framearticle/dto/FrameArticleRequestDto.java
@@ -13,18 +13,21 @@ import javax.validation.constraints.Size;
 @Getter
 @NoArgsConstructor
 public class FrameArticleRequestDto {
+    @Size(min=3, max=100, message="프레임 이름은 최소 3글자, 100글자 이하로 작성하세요")
     @NotBlank(message = "프레임 이름을 입력해주세요")
-    @Size(min=3, max=100, message="제목은 최소 3글자, 100글자 이하로 작성하세요")
     private String name;
+
     @Size(min=3, max=1000, message="링크 길이가 최대 1000글자입니다.")
     @NotBlank(message = "Link를 입력해주세요")
     private String link;
+
     @NotNull(message="허용 여부를 선택해주세요")
     private Boolean isPublic;
 
     @NotNull(message="규격을 선택해주세요")
     private FrameSpecification frameSpecification;
 
+    @Size(min=3, max=20, message="제목은 최소 3글자, 20글자 이하로 작성하세요")
     @NotBlank(message = "제목을 입력해주세요")
     private String subject;
 
