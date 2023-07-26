2b562af8bb8dc4c78f621c1fa66d18109e904035
김세혁 <se6816@naver.com>
2023-07-26 12:23:46 +0900

    Change: FrameArticleRequestDto @Vaild 제약 조건 변경

--- BackEnd/article/src/main/java/com/reminiscence/article/framearticle/dto/FrameArticleRequestDto.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/framearticle/dto/FrameArticleRequestDto.java
@@ -8,12 +8,15 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class FrameArticleRequestDto {
-    @NotBlank(message = "이름을 입력해주세요")
+    @NotBlank(message = "프레임 이름을 입력해주세요")
+    @Size(min=3, max=100, message="제목은 최소 3글자, 100글자 이하로 작성하세요")
     private String name;
+    @Size(min=3, max=1000, message="링크 길이가 최대 1000글자입니다.")
     @NotBlank(message = "Link를 입력해주세요")
     private String link;
     @NotNull(message="허용 여부를 선택해주세요")
