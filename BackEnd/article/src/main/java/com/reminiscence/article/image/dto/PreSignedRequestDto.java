21ecd4540aaff2d15c5f093389541663a6681bb1
이찬형 <cksgud410@gmail.com>
2023-07-26 14:43:13 +0900

    Change : 파라미터 유효성 검증을 위한 @Vaild 추가

--- BackEnd/article/src/main/java/com/reminiscence/article/image/dto/PreSignedRequestDto.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/image/dto/PreSignedRequestDto.java
@@ -3,10 +3,16 @@ package com.reminiscence.article.image.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class PreSignedRequestDto {
+    @NotBlank
     private String imageName;
+
+    @NotNull
     private ImageType imageType;
 
     public PreSignedRequestDto(String imageName, ImageType imageType) {
