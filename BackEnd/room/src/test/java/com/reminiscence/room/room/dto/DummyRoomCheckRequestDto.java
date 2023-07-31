572b27983e50bfda6742635add910da170503b02
이찬형 <cksgud410@gmail.com>
2023-07-31 13:34:00 +0900

    Test : 방 생성/삭제/접근 테스트 추가

---
+++ BackEnd/room/src/test/java/com/reminiscence/room/room/dto/DummyRoomCheckRequestDto.java
@@ -0,0 +1,22 @@
+package com.reminiscence.room.room.dto;
+
+public class DummyRoomCheckRequestDto {
+    private String password;
+
+    public static class Builder{
+        private String password;
+
+        public Builder password(String password){
+            this.password = password;
+            return this;
+        }
+
+        public DummyRoomCheckRequestDto build(){
+            return new DummyRoomCheckRequestDto(this);
+        }
+    }
+
+    public DummyRoomCheckRequestDto(DummyRoomCheckRequestDto.Builder builder){
+        this.password = builder.password;
+    }
+}
