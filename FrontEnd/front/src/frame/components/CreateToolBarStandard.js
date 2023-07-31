08f473933d64f2d42d6fccf523455be9b652e6ff
s01k1m <sk618dev@gmail.com>
2023-08-01 00:54:37 +0900

    Comment: 컴포넌트 설명 추가 및 콘솔로그 삭제

--- FrontEnd/front/src/frame/components/CreateToolBarStandard.js
+++ FrontEnd/front/src/frame/components/CreateToolBarStandard.js
@@ -1,3 +1,4 @@
+// 프레임 생성할 때 프레임의 규격을 정하는 툴바의 디테일 컴포넌트입니다.
 import React from "react";
 import { useDispatch } from "react-redux";
 import { Resize } from "../../redux/features/frame/frameSlice";
@@ -9,14 +10,12 @@ export default function Standard() {
     // 세로로 589.6px, 가로로 207.8px 크기로 변경
     const payload = { width: 207.8, height: 589.6 };
     dispatch(Resize(payload));
-    console.log("세로");
   };
 
   const handleHorizontalResize = () => {
     // 가로로 589.6px, 세로로 400.6px 크기로 변경
     const payload = { width: 589.6, height: 400.6 };
     dispatch(Resize(payload));
-    console.log("가로");
   };
 
   return (
