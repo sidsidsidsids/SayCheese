b12e5f0c55a5ba3a9175f8404fb66f7d3033a43f
이찬형 <cksgud410@gmail.com>
2023-07-31 13:38:25 +0900

    Test : 방 참가자 테스트 코드 기본 형태 생성

---
+++ BackEnd/room/src/test/java/com/reminiscence/room/participant/ParticipantTest.java
@@ -0,0 +1,12 @@
+package com.reminiscence.room.participant;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+public class ParticipantTest {
+
+}
