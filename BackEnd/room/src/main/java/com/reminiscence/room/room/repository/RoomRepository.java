5e2b16ec739e490cd3896a7fa13da3bf69274af1
이찬형 <cksgud410@gmail.com>
2023-08-11 13:11:28 +0900

    Change : RoomRepositoryCustom 추가

--- BackEnd/room/src/main/java/com/reminiscence/room/room/repository/RoomRepository.java
+++ BackEnd/room/src/main/java/com/reminiscence/room/room/repository/RoomRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long> , RoomRepositoryCustom{
     public Optional<Room> findByRoomCode(String roomCode);
 }
