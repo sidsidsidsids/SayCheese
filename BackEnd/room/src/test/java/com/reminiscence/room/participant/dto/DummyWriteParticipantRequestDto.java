102184de27640cf0a0e249868f2e4ef59f2ce4c7
이찬형 <cksgud410@gmail.com>
2023-08-14 18:30:54 +0900

    Test : 참여자 생성 Request 컬럼 변경 설정

--- BackEnd/room/src/test/java/com/reminiscence/room/participant/dto/DummyWriteParticipantRequestDto.java
+++ BackEnd/room/src/test/java/com/reminiscence/room/participant/dto/DummyWriteParticipantRequestDto.java
@@ -1,40 +1,11 @@
 package com.reminiscence.room.participant.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class DummyWriteParticipantRequestDto {
 
-    private Character ownerYn;
     private String roomCode;
-
-    public static class Builder{
-        private Character ownerYn;
-        private String roomCode;
-
-
-        public Builder ownerYn(Character ownerYn){
-            this.ownerYn = ownerYn;
-            return this;
-        }
-
-        public Builder roomCode(String roomCode){
-            this.roomCode = roomCode;
-            return this;
-        }
-
-        public DummyWriteParticipantRequestDto build(){
-            return new DummyWriteParticipantRequestDto(this);
-        }
-    }
-
-    private DummyWriteParticipantRequestDto(DummyWriteParticipantRequestDto.Builder builder){
-        this.ownerYn = builder.ownerYn;
-        this.roomCode = builder.roomCode;
-    }
-
-    public Character getOwnerYn() {
-        return ownerYn;
-    }
-
-    public String getRoomCode() {
-        return roomCode;
-    }
 }
