cb68316086a6353eff818627afbf6d1fdbadbfc1
gofoxfox <gofoxfox@naver.com>
2023-08-14 18:14:45 +0900

    Fix: data.sql 충돌 수정

---
+++ BackEnd/user/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into member (email, password, nickname, role, age, name, del_yn, gender_fm, personal_agreement_yn, created_date)
+values ('user12@naver.com', '1234', 'piggy12', 'ADMIN', 31, 'Tom', 'N', 'M', 'Y', now());
+
+insert into member (email, password, role, nickname, age, name, del_yn, gender_fm, personal_agreement_yn, created_date)
+values ('go12@naver.com', 'SEOUL', 'ADMIN', 'PIG12', 33, 'Jerry', 'N', 'F', 'Y', now());
