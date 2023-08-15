f51757bb8f931d6bcee0290dfdd8a8d957f5825e
gofoxfox <gofoxfox@naver.com>
2023-08-15 19:51:18 +0900

    Change: my page 내 페이지당 frame 수 6개 -> 9개 수정

--- BackEnd/article/src/main/java/com/reminiscence/article/common/Pagination.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/common/Pagination.java
@@ -1,6 +1,7 @@
 package com.reminiscence.article.common;
 
 public class Pagination {
+    public final static int DEFAULT_MY_FRAME_PER_PAGE_SIZE=9; // 마이페이지 프레임 한 페이지에 있는 데이터 수
     public final static int DEFAULT_FRAME_PER_PAGE_SIZE=6; // 프레임 한 페이지에 있는 데이터 수
     public final static int DEFAULT_NOTICE_PER_PAGE_SIZE=10; // 공지 한 페이지에 있는 데이터 수
     public final static int DEFAULT_QNA_PER_PAGE_SIZE=10; // Faq 한 페이지에 있는 데이터 수
