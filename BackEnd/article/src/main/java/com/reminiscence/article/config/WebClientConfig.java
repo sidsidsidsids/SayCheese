67acd426feee636b1564726f5f68cf7fd796ef1a
이찬형 <cksgud410@gmail.com>
2023-08-17 13:55:29 +0900

    Change : 도메인 환경 변수로 설정

--- BackEnd/article/src/main/java/com/reminiscence/article/config/WebClientConfig.java
+++ BackEnd/article/src/main/java/com/reminiscence/article/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.reminiscence.article.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,13 +8,18 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.validation.Valid;
+
 @Configuration
 public class WebClientConfig {
+
+    @Value("${spring.data.base-url}")
+    private String BASE_URL;
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("https://i9a401.p.ssafy.io")
+                .baseUrl(BASE_URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
